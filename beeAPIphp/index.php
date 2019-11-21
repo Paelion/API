244e10e5c4a25752d6d46151d4519873c7b20420
Paelion <aelion.pauline1@gmail.com>
2019-11-21 12:41:48 +0100

    api
    api

---
+++ beeAPIphp/index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+
+
+<!-- HEAD -->
+<head>
+    <title>Bee-learning</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="shortcut icon" type="image/jpg" href="img/icon.jpg"/>
+</head>
+
+<!-- Navigation -->
+<body>
+
+<!-- Header -->
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">Bee</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#lifestyle" style="font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Lifestyle</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#ruche" style="font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Rûche</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#reine" style="font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Reine</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#cause" style="font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Causes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#galerie" style="font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Galerie</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+</header>
+
+<!-- Content -->
+
+<div class="container-fluid pl-0 pr-0">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/image6.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/image4.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/image3.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <div class="container-fluid pl-0 pr-0" id="lifestyle">
+        <center><h2 style="font-size: 40px; padding-top: 40px; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Lifestyle</h2></center>
+        <p style="font-size:23px; padding: 30px 75px; text-align: justify; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Les abeilles vivent en colonie. Elles
+            forment une société très organisée, un peu comme une grande entreprise.
+            Autour de la reine, dont la tâche unique est de pondre et pondre encore, jusqu’à 50 000 ouvrières s’activent
+            avec ardeur. Dans la ruche, seules les quelques centaines de faux-bourdons paressent ! Durant leur
+            existence, les abeilles exercent jusqu’à sept fonctions différentes : nettoyeuse, nourrice, architecte,
+            manutentionnaire, ventileuse, gardienne et butineuse.
+            elles vivent de 30 à 45 jours pour les abeilles au printemps et en été,
+            plusieurs mois pour celles qui naissent à l’automne et permettent à la colonie de survivre à l’hiver et
+            redémarrer le cycle.
+        </p>
+    </div>
+</div>
+
+
+
+
+
+
+
+<div class="container-fluid pl-0 pr-0" id="ruche">
+    <div class="row no-gutters">
+        <div class="col-12 col-md-6  pl0 pr0">
+            <img src="img/ruche.jpg" style="width: 100%">
+        </div>
+        <div class="col-12 col-md-6" style="background-color: #212529; color: white">
+            <center><h2 style="font-size: 40px; padding-top: 40px; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">La Rûche</h2></center>
+            <p style="font-size: 23px; padding: 30px 70px 60px 70px; text-align: justify; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif; margin-bottom: 0px">Une ruche est une structure presque
+                fermée abritant une colonie d'abeilles. L’intérieur de la ruche est composé de rayons formés par des
+                cellules hexagonales de cire d'abeille. Les abeilles utilisent ces cellules pour le stockage de la
+                nourriture (miel et pollen), et pour le renouvellement de la population (œufs, larves et nymphes).
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid pl-0 pr-0" id="reine">
+    <div class="row no-gutters">
+        <div class="col-12 col-md-6">
+            <center><h2 style="font-size: 40px; padding-top: 40px; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">La Reine</h2></center>
+            <p style="font-size: 23px; padding: 30px 70px 60px 70px; text-align: justify; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">
+                La gelée royale est une substance produite par les abeilles ouvrières, grâce à des glandes contenues
+                dans leur tête. Elle est gélatineuse, de couleur blanchâtre.
+                La gelée royale apporte à la larve une substance qui permet de modifier son ADN pour en faire une
+                abeille reine et non ouvrière.
+            </p>
+        </div>
+        <div class="col-12 col-md-6 pl0 pr0">
+            <img src="img/queen.jpg" style="width: 100%">
+        </div>
+    </div>
+
+    <div>
+        <div class="container-fluid pl-0 pr-0" id="cause">
+            <center><h2 style="font-size: 40px; padding-top: 40px;background-color: #212529; color: white; margin-bottom: 0; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Les Causes</h2></center>
+            <p style="font-size:23px; padding: 30px 75px; text-align: justify; background-color: #212529; color: white; margin-bottom: 0; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">
+                Nous avons été sensibilisés au rôle des abeilles dans l'écosystème. En effet, les abeilles favorisent la
+                pollinisation des plantes sauvages et cultivées et elles sont sensibles aux traitements phytosanitaires.
+                Depuis quelques années le sort des abeilles est devenu inquiétant : leur taux de surmortalité atteint 30
+                à
+                35 %, taux anormalement élevé qui atteint dans certains cas 50 % de pertes hivernales. Sur le banc des
+                accusés : multiplication des substances chimiques dans l'environnement, présence de parasites, effets
+                désastreux des monocultures, ravages du frelon asiatique, impact du changement climatique
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid pl-0 pr-0" id="progress" style="padding: 40px; margin-left: 13%; width: 80%">
+    <div class="use">
+        <p style="margin-bottom: 10px; font-size: 20px; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">
+            Utilité de l'espèce :</p>
+    </div>
+    <div class="progress" style="width: 90%;">
+        <div class="progress-bar bg-warning" role="progressbar" style="width: 100%" aria-valuenow="25" aria-valuemin="0"
+             aria-valuemax="100"></div>
+    </div>
+
+    <div class="danger" style="padding-top: 30px;">
+        <p style="margin-bottom: 10px; font-size: 20px; font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">
+            Risque d'extinction :</p>
+    </div>
+    <div class="progress" style="width: 90%;">
+        <div class="progress-bar bg-warning" role="progressbar" style="width: 80%" aria-valuenow="100" aria-valuemin="0"
+             aria-valuemax="100">
+        </div>
+    </div>
+
+    <div class="hiver" style="padding-top: 30px;">
+        <p style="margin-bottom: 10px; font-size: 20px;font-family: trebuchet ms,arial,tahoma,verdana,sans-serif">Taux
+            de mortalité durant l'hiver :</p>
+    </div>
+    <div class="progress" style="width: 90%;">
+        <div class="progress-bar bg-warning" role="progressbar" style="width: 50%" aria-valuenow="75" aria-valuemin="0"
+             aria-valuemax="100"></div>
+    </div>
+</div>
+
+<div class="galerie row" id="galerie" style="background-color: #212529; color: white; padding: 40px;">
+    <h2>Galerie</h2>
+    <div class="galerieVideos" style="display: flex">
+    <?php
+    $API_key    = 'AIzaSyDJLp29i0mJkck4imBSpmVqUh9Uh99V9lE';
+    $channelID  = 'UCYDdtHumvL5mnx0Hk5HDoag';
+    $maxResults = 10;
+
+    $videoList = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$channelID.'&maxResults='.$maxResults.'&key='.$API_key.''));
+
+    foreach($videoList->items as $item){
+        if(isset($item->id->videoId)){
+            echo '<div class="youtube-video col-2" style="display:block;padding:10px;">
+        <iframe width="280" height="150" src="https://www.youtube.com/embed/'.$item->id->videoId.'" frameborder="0" allowfullscreen></iframe>
+        <h2 style="font-size: 15px">'. $item->snippet->title .'</h2>
+    </div>';
+        }
+    }
+    ?>
+        </div>
+</div>
+
+
+
+<!-- Footer -->
+<footer style="background-color: #FFC107; height: 50px;display: flex;justify-content: center;padding-top: 10px;">
+    <a href="" style="color:black;margin-right: 20px;">Mentions legales</a>
+    <p>©2019 LuPauRay</p>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+</footer>
+
+</body>
+</html>
